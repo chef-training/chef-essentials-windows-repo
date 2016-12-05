5a73c418b0f8b9ea7e6954e355ad357fbcff1f4f
johnfitzpatrick <jhnftzptrck@gmail.com>
2016-12-05 21:07:22 +0400

    Update server.rb
    Fixed error in server.rb recipe

--- cookbooks/myiis/recipes/server.rb
+++ cookbooks/myiis/recipes/server.rb
@@ -1,14 +1,11 @@
-powershell_script 'Install IIS' do
-  code 'Add-WindowsFeature Web-Server'
-  not_if 'Get-WindowsFeature Web-Server'
-end
-
-directory 'C:\inetpub\wwwroot'
-
-file 'C:\inetpub\wwwroot\Default.htm' do
-  content '<h1>Hello, world!</h1>'
-end
-
-service 'w3svc' do
-  action [:enable, :start]
+powershell_script 'Install IIS' do		
+  code 'add-windowsfeature Web-Server'		
+end		
+		
+template 'c:\inetpub\wwwroot\Default.htm' do		
+  source 'Default.htm.erb'		
+end		
+		
+service 'w3svc' do		
+  action [:enable, :start]		
 end
